95430829a4eb050a20a9131ce6da31b26dfaedd8
susanaviruglio <>
2023-08-21 12:56:08 +0000

    added responsive navigation bar, fixed errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
-let navbar = document.querySelector('.navbar');
-
-document.querySelector('#menu-btn').onclick = () => {
-    navbar.classList.toggle('active');
-};
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
