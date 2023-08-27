a11dfe70b409be9713b40abd775e310c77d4d19e
susanaviruglio <>
2023-08-27 15:34:26 +0000

    added function with event listener for the images in javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,27 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
+
+/* Memory game here--------------------------------------------*/
+
+/* function to rotate the cards by click , add click event to all the cards--------------------*/
+document.addEventListener('DOMContentLoaded', function(){
+    let clicks = document.getElementsByClassName('square');
+    for (let click of clicks) {
+        click.addEventListener('click', function(){
+            alert('you click on me!');
+    })
+ }
+})
+
+/* check if the cards match ----------------------------*/
+/* check answer if they match correct point*/
+/* check wrong add a mistake*/
+/*mix all the cards*/
 
