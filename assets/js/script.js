95887cbdc81b02326d97279a6f7052f4cef2c427
susanaviruglio <>
2023-08-29 12:09:46 +0000

    improve function using queryselector and forEach it was getting very complicated and it was not working the previous one

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,56 +8,24 @@ function myFunction() {
     }
 }
 
-/* Memory game here--------------------------------------------*/
 
-/* function to rotate the cards by click , add click event to all the cards--------------------*/
 
-document.addEventListener('DOMContentLoaded', function(){
-    let clicks = document.getElementsByClassName('square');
-    for (let click of clicks) {
-        click.addEventListener('click', function(){
-            if (this !== cardOne) {
-                let backs = document.getElementsByClassName('card');
-                for (let back of backs) {
-                    this.classList.add('flip');
-                    /*if(!cardOne){
-                        return cardOne = this;
-                    }
-                    cardTwo = this;
-                    let cardOneImg = cardOne.querySelector('card-back').children.src;
-                    let cardTwoImg = cardTwo.querySelector('card-back');
-                    console.log(cardTwoImg.src);
-                    matchingCards(cardOneImg, cardTwoImg);*/
-                }   
-            }     
-    })
- }
-})
+/*Memory game stars here*/
 
-/* check if the cards match ----------------------------*/
-let cardOne = document.getElementsByClassName('card-back');
-for (let i = 0; i < main.length; i++) {
-    console.log(cardOne[i].children);
+/*query selector all*/
+
+const games = document.querySelectorAll('.square');
+/* for each method to select all in an array*/
+games.forEach(game => game.addEventListener('click', memoryGame));
+/*match cards using comparison*/
+function memoryGame() {
+  this.classList.add('flip');
+  if(!flips) {
+    flips = false;
+    cardOne = this;
+  }
 }
-let cardTwo = cardOne;
 
 
-/*function matchingCards(img1,img2){
-    if (img1 === img2) {
-      alert('matched!')
-        console.log('card matched!');
-    } 
-    console.log('card not matched!'); 
-}*/
-
-
-/*let cardOne, cardTwo = document.getElementsByClassName('card-back');
-let backs = document.getElementsByClassName('card-front');
-for (let back of backs) {
-    this.classList.add('flip');
-}*/
-
-/* check answer if they match correct point*/
-/* check wrong add a mistake*/
-/*mix all the cards*/
-
+let cardOne, cardTwo;
+let flips = true;
