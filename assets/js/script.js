9499b1b79021a89675e039e93473fbc724f17756
susanaviruglio <>
2023-09-11 19:16:18 +0000

    modified function backImg the cards were not shuffle properly, I received support from my tutor online and my error was in my line 94 in javascrip I had to change my src code from double[shuffleBacj] to numberCards

--- assets/js/script.js
+++ assets/js/script.js
@@ -77,48 +77,34 @@ let game = questionImg();
 
 
 // cards hidden underneath
-// button game in html: id="buttoncolor"
-const backCard = ['assets/images/img1.png', 'assets/images/img2.png', 'assets/images/img3.png',
-    'assets/images/img4.png', 'assets/images/img5.png', 'assets/images/img6.png',
-    'assets/images/img7.png', 'assets/images/img8.png'];
-const doubleCard = [...backCard, ...backCard];
-
-let gameLength = doubleCard.length;
-
-
-for (let i = 0; i < gameLength; i++) {
-    let shuffleBack = Math.floor(Math.random() * doubleCard.length);
-    let numberCards = doubleCard[shuffleBack];
 
 
 
-    let newBack = document.createElement('img');
-    newBack.setAttribute('src', backCard[shuffleBack]);
-    newBack.setAttribute('alt', 'Coffee image');
-    //newBack.style.visibility = 'hidden';
-    document.getElementById('back-game').appendChild(newBack);
+function backImg() {
+    const backCard = ['assets/images/img1.png', 'assets/images/img2.png', 'assets/images/img3.png',
+        'assets/images/img4.png', 'assets/images/img5.png', 'assets/images/img6.png',
+        'assets/images/img7.png', 'assets/images/img8.png'];
+    const doubleCard = [...backCard, ...backCard];
 
+    for (let i = 0; i < backCard.length * 2; i++) {
+        let shuffleBack = Math.floor(Math.random() * doubleCard.length); //get a random index
+        let numberCards = doubleCard[shuffleBack]; // use this index to pick a random card
 
-}
+        let newBack = document.createElement('img');
+        newBack.setAttribute('src', numberCards); //use the chosen card as the src
+        newBack.setAttribute('alt', 'Coffee image');
+        document.getElementById('back-game').appendChild(newBack);
 
-
-
-function backImg(event) {
-    let shuffleBack = document.getElementById('back-game').children;
-
-    for (let i = 0; i < shuffleBack.length; i++) {
-        let randomCards = Math.floor(Math.random() * shuffleBack.length);
-        let pictures = shuffleBack[randomCards];
-        //images need to repeat at least 1 time
-
-        console.log(pictures);
+        doubleCard.splice(shuffleBack, 1); // Remove the used card from the doubleCard array
 
     }
 
 }
 
-let button = document.getElementById('buttoncolor');
-button.addEventListener('click', backImg);
+let gameShuffle = backImg();
+
+
+
 
 
 
@@ -126,14 +112,15 @@ button.addEventListener('click', backImg);
 // I created a button to start the game
 
 
+function buttonColor(event) {
+    button.style.backgroundColor = "#dcf9fe";
+    button.innerHTML = "Reset Game";
+    button.style.color = '#d91979';
+    button.style.borderColor = '#0d4953';
+}
 
-/*button.style.backgroundColor = "#dcf9fe";
-button.innerHTML = "Reset Game";
-button.style.color = '#d91979';
-button.style.borderColor = '#0d4953';*/
-
-
-
+let button = document.getElementById('buttoncolor');
+button.addEventListener('click', buttonColor);
 
 
 
